2c395ccdce76876f1cdf00719bc312b5671eec6a
Charles Odili <chaluwa@gmail.com>
2020-10-17 21:57:05 +0100

    add API versioning

--- src/app.js
+++ src/app.js
@@ -9,17 +9,18 @@ import endpoints from './endpoints';
 
 // TODO add API versioning
 const app = express();
+const APIVersion = process.env.APIVERSION;
 
 const apiMonitor = moesif({
   applicationId: process.env.MOESIFID
 });
 
 const apiSpec = path.join(__dirname, 'api-spec.yaml');
-app.use('/spec', express.static(apiSpec));
+app.use(`/${APIVersion}/spec`, express.static(apiSpec));
 app.use(cors());
 app.use(apiMonitor);
 app.use(express.json());
-app.use(pino({ useLevel: 'info' }));
+// app.use(pino({ useLevel: 'info' }));
 app.use(
   APIValidator.middleware({
     apiSpec,
@@ -27,15 +28,10 @@ app.use(
   })
 );
 
-app.use('/register', endpoints.register);
+app.use(`/${APIVersion}`, endpoints.hello);
+app.use(`/${APIVersion}/ping`, endpoints.ping);
+app.use(`/${APIVersion}/register`, endpoints.register);
 
-const defaultResponse = 'Welcome to the EffesEYE API. Consult the documentation';
-app.get('/', (req, res) => res.status(200).send({ message: defaultResponse }));
-app.post('/', (req, res) => res.status(200).send({ message: defaultResponse }));
-app.get('/ping', (req, res) => res.status(200).send({ message: `EffesEYE API: ${new Date()}` }));
-app.post('/ping', (req, res) => res.status(200).send({ message: `EffesEYE API: ${new Date()}` }));
-app.get('*', (req, res) => res.status(400).json({ message: 'Invalid Destination' }));
-app.post('*', (req, res) => res.status(400).json({ message: 'Invalid Destination' }));
 
 app.use((err, req, res, next) => {
   // TODO log this to the API monitoring service
