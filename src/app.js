0f76620ff142e8df9be6bd6e822c3b05288a821c
Charles Odili <chaluwa@gmail.com>
2020-10-17 13:09:31 +0100

    enable simple  logging

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(apiMonitor);
 app.use(express.json());
-// app.use(pino());
+app.use(pino({ useLevel: 'error' }));
 
 app.use('/register', endpoints.register);
 
