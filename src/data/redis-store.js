c1f1bca7e8355135bd67f18a31ba39b3b756058a
Charles Odili <chaluwa@gmail.com>
2020-10-18 12:18:15 +0100

    cleanup .env variable names

--- src/data/redis-store.js
+++ src/data/redis-store.js
@@ -1,4 +1,4 @@
 import IORedis from 'ioredis';
 
-const Redis = new IORedis(`${process.env.REDISEndpoint}`);
+const Redis = new IORedis(`${process.env.REDIS_ENDPOINT}`);
 export default Redis;
--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import jwt from 'express-jwt';
 import register from './register';
 
 const ping = Router();
 const hello = Router();
 const defaultResponse = `EffesEYE API: ${new Date()}`;
+
+const authenticate = jwt({
+  algorithms: ['HS256'],
+  secret: process.env.AUTH_SECRET,
+  expiresIn: 60 * 30 // expire/refresh the token after 30 mins
+});
+
+register.use(authenticate);
 
 ping.get('/', (req, res) => res.status(200).send({ message: defaultResponse }));
 ping.post('/', (req, res) => res.status(200).send({ message: defaultResponse }));
